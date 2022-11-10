ce174eeca388799e44717ac29d3933d6e84741f4
duynguyenzno <72838818+duynguyenzno@users.noreply.github.com>
2022-11-10 16:23:59 +0700

    demo

--- orther.js
+++ orther.js
@@ -5,5 +5,9 @@ app.get("/", (req, res) => {
   res.send("");
 });
 
+app.get("/", (req, res) => {
+  res.send("HELLO");
+});
+
 const Port = process.env.Port || 3000;
 app.listen(Port, () => console.log("Server running on port ${Port}"));
