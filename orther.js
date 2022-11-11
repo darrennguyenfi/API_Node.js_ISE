fd2dbc247b64dba1cb9fe5bd43de0c3171d18922
duynguyenzno <72838818+duynguyenzno@users.noreply.github.com>
2022-11-11 13:16:03 +0700

    get API 2

--- orther.js
+++ orther.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("");
-});
+const courses = [
+  { id: 1, name: "Node.js" },
+  { id: 2, name: "GOLang" },
+  { id: 3, name: "ReactJS" },
+];
 
 app.get("/", (req, res) => {
   res.send("HELLO");
 });
+
+app.get("/api/course", (req, res) => {
+  res.send(courses);
+});
+
+// app.get("/api/course/:id", (req, res) => {
+//   const course = courses.find(
+//     (courses) => courses.id === parseInt(req.params.id)
+//   );
+// });
 
 const Port = process.env.Port || 3000;
 app.listen(Port, () => console.log("Server running on port ${Port}"));
